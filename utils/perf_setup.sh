0a5a64b94f287a55b8a987e76fda4aa348407be3
Xiangfeng Zhu <xzhu0027@gmail.com>
2022-08-01 14:17:26 -0700

    add perf setup script

---
+++ utils/perf_setup.sh
@@ -0,0 +1,33 @@
+# Setup script for modified perf verison
+
+VERSION=5.4.156
+wget -O linux.tar.gz wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x//linux-$VERSION.tar.gz
+
+sudo apt-get update
+sudo apt-get install libiberty-dev binutils-dev
+
+mkdir -p linux
+tar -xf linux.tar.gz -C linux
+
+cd linux/linux-$VERSION
+
+#sed -i '47,50d' ./tools/perf/jvmti/jvmti_agent.c
+
+#patch -p1 < ../../perf-patch-5.4-conv.patch
+#patch -p1 < ../../binutils.patch
+#git apply ../../perf-patch-5.4-conv.patch --verbose
+#git apply ../../perf-patch-5.4.patch
+#git apply ../../binutils.patch
+
+
+#PYTHON=python3 make -C tools/perf install
+
+cd tools/perf/
+make clean && make -j 8
+
+cd ../../../../
+
+mkdir -p perf/
+cp -r linux/linux-$VERSION/tools/perf/* perf/
+#rm -r linux/
+#rm -r linux.tar.gz
