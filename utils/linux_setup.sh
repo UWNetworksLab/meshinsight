dd182ce465e4e44f63d2b1d56a90966a1c6aee58
Xiangfeng Zhu <xzhu0027@gmail.com>
2022-11-17 23:31:00 -0800

    update linux benchmark setup script

--- utils/linux_setup.sh
+++ utils/linux_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Disable TurboBoost
-cat /sys/devices/system/cpu/intel_pstate/no_turbo
+# cat /sys/devices/system/cpu/intel_pstate/no_turbo
 echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 
 # Disable CPU Frequency Scaling 
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+# cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
 # Disable CPU Idle State
-sudo cpupower frequency-info
+# sudo cpupower frequency-info
 sudo cpupower idle-set -D 0
 
 # Disable address space randomization 
