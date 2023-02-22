5ae9df5234413e976a20ec215277acb481f9ebb0
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-02-21 22:14:39 -0800

    add proxyless grpc benchmark

---
+++ meshinsight/profiler/benchmark/echo_server_grpc_proxyless/go.mod
@@ -0,0 +1,6 @@
+module github.com/echo_server_grpc_proxyless
+
+go 1.18
+
+
+replace github.com/Romero027/echo-server-grpc/pb => ./pb
---
+++ meshinsight/profiler/benchmark/echo_server_grpc_proxyless/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+
+	echo "github.com/UWNetworksLab/meshinsight/meshinsight/profiler/benchmark/echo_server_grpc_proxyless/pb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedEchoServiceServer
+}
+
+func (s *server) Echo(ctx context.Context, x *echo.Msg) (*echo.Msg, error) {
+	log.Printf("got: [%s]", x.GetBody())
+	return x, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	fmt.Printf("Starting server at port 9000\n")
+
+	echo.RegisterEchoServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
