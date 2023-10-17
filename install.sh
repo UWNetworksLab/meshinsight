cc51d4eacf0586f6fd29f8814eb4e35e65413c10
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-10-17 14:35:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,10 +57,10 @@ sudo apt update
 version=$(lsb_release -r | awk '{print $2}')
 if [ $(echo "$version == 22.04" | bc) -eq 1 ]; then
   # Execute command for Ubuntu 22.04
-  sudo apt install -y bison build-essential cmake flex git libedit-dev libllvm14 llvm-14-dev libclang-14-dev python3 zlib1g-dev libelf-dev libfl-dev python3-distutils
+  sudo apt install -y zip bison build-essential cmake flex git libedit-dev libllvm14 llvm-14-dev libclang-14-dev python3 zlib1g-dev libelf-dev libfl-dev python3-setuptools liblzma-dev libdebuginfod-dev arping netperf iperf
 else
   # Execute command for Ubuntu 20.04
-  sudo apt install -y bison build-essential cmake flex git libedit-dev libllvm11 llvm-11-dev libclang-11-dev python zlib1g-dev libelf-dev libfl-dev python3-distutils
+  sudo apt install -y zip bison build-essential cmake flex git libedit-dev libllvm12 llvm-12-dev libclang-12-dev python zlib1g-dev libelf-dev libfl-dev python3-setuptools liblzma-dev arping netperf iperf
 fi
 
 # Delete if installed
